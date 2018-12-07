f1d8af9f2e4d9aef54b04c88a6322414df645c7c
chrismarm <chrismarm.development@gmail.com>
2018-12-07 20:55:15 +0100

    Errors sorted out

--- cluster_install.sh
+++ cluster_install.sh
@@ -10,8 +10,17 @@ gcloud container clusters create sample-cloudbuild --machine-type n1-standard-2 
 # Auto-configure cluster correctly
 gcloud container clusters get-credentials sample-cloudbuild
 
+PROJECT="$(gcloud projects describe \
+    $(gcloud config get-value core/project -q) --format='get(projectNumber)')"
+
+gcloud projects add-iam-policy-binding $PROJECT \
+    --member=serviceAccount:$PROJECT@cloudbuild.gserviceaccount.com \
+    --role=roles/container.developer
+
 # Add our GCP account as cluster administrator through a new binding
 #kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+
+
 
 # Creation of a service account in the kube-system namespace
 #kubectl create -f serviceaccount.yaml
